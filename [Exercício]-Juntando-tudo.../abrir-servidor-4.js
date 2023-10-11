8fd8dcaedc0848f46449073f77e81f8d85b86199
3LieLs <3lielfernandesribeiro@gmail.com>
2023-10-11 12:04:42 -0300

    att

--- [Exercício]-Juntando-tudo.../abrir-servidor-4.js
+++ [Exercício]-Juntando-tudo.../abrir-servidor-4.js
@@ -10,25 +10,105 @@ app.get("/", (request, response) => {
     response.render("index"); // views/index.ejs
 });
 
-app.get("/adicao", (request, response) => {
-    val_num1 = request.query.num1
-    val_num2 = request.query.num2
-    resultado = parseFloat(val_num1) + parseFloat(val_num2)
-    response.send(
-        `<div style="
-        position: absolute;
-        background-color: orangered;
-        border: solid 8px black;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 50%;
-        height: 90%;
-        text-align: center;
-        font-size: 200%;">
-    <p>Resultado:<br>${val_num1} + ${val_num2} = ${resultado}</p>
-</div>`
-    );
+app.get("/conta", (request, response) => {
+    const data = {
+        val_num1: request.query.num1,
+        val_num2: request.query.num2,
+        val_operacao: request.query.operacao
+    }
+
+    if (data.val_operacao == '+') {
+        data.resultado = parseFloat(data.val_num1) + parseFloat(data.val_num2)
+        response.send(
+            `<div style="
+            position: absolute;
+            background-color: orangered;
+            border: solid 8px black;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 50%;
+            height: 90%;
+            text-align: center;
+            font-size: 200%;">
+        <p>Resultado:<br>${data.val_num1} + ${data.val_num2} = ${data.resultado}</p>
+    </div>`
+        );
+    }
+    if (data.val_operacao == '-') {
+        data.resultado = parseFloat(data.val_num1) - parseFloat(data.val_num2)
+        response.send(
+            `<div style="
+            position: absolute;
+            background-color: orangered;
+            border: solid 8px black;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 50%;
+            height: 90%;
+            text-align: center;
+            font-size: 200%;">
+        <p>Resultado:<br>${data.val_num1} - ${data.val_num2} = ${data.resultado}</p>
+    </div>`
+        );
+    }
+    if (data.val_operacao == '*') {
+        data.resultado = parseFloat(data.val_num1) * parseFloat(data.val_num2)
+        response.send(
+            `<div style="
+            position: absolute;
+            background-color: orangered;
+            border: solid 8px black;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 50%;
+            height: 90%;
+            text-align: center;
+            font-size: 200%;">
+        <p>Resultado:<br>${data.val_num1} * ${data.val_num2} = ${data.resultado}</p>
+    </div>`
+        );
+    }
+    if (data.val_operacao == '/') {
+        data.resultado = parseFloat(data.val_num1) / parseFloat(data.val_num2)
+        response.send(
+            `<div style="
+            position: absolute;
+            background-color: orangered;
+            border: solid 8px black;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 50%;
+            height: 90%;
+            text-align: center;
+            font-size: 200%;">
+        <p>Resultado:<br>${data.val_num1} / ${data.val_num2} = ${data.resultado}
+        <br>Resto: ${data.val_num1 % data.val_num2}</p>
+    </div>`
+        );
+    }
+    else{
+        data.resultado = parseFloat(data.val_num1) - parseFloat(data.val_num2)
+        response.send(
+            `<div style="
+            position: absolute;
+            background-color: orangered;
+            border: solid 8px black;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            width: 50%;
+            height: 90%;
+            text-align: center;
+            font-size: 200%;">
+        <p>Resultado:<br>Não foi possivel</p>
+    </div>`
+        );
+    }
+    
 });
 
 app.use((request, response, next) => {
