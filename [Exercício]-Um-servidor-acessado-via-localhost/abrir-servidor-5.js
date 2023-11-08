1eb6b4214d167080acda7f2e708c356094bedf88
3LieLs <3lielfernandesribeiro@gmail.com>
2023-11-08 10:04:46 -0300

    att

---
+++ [Exercício]-Um-servidor-acessado-via-localhost/abrir-servidor-5.js
@@ -0,0 +1,27 @@
+const port = 8020;
+var cont = 0
+
+//Express
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get("/", (request, response) => {
+    response.render("index"); // views/index.ejs
+});
+
+app.get("/objetivos", (request, response) => {
+    response.render("objetivos");
+});
+
+app.get("/tec", (request, response) => {
+    response.render("tec");
+});
+
+app.use((request, response, next) => {
+    response.status(404).send("<h1>Página não encontrada.</h1>");
+})
+
+app.listen(port, () => {
+    console.log(`Servidor funcionando na porta: ${port}`);
+});
