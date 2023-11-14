93d6bf3dba81abe87ae73e21b2656ccba92c7b4f
3LieLs <3lielfernandesribeiro@gmail.com>
2023-11-14 18:09:56 -0300

    att

---
+++ [Exercicio]-Servidor-Node.js-com-Express-e-EJS/abrir-servidor(1).js
@@ -0,0 +1,37 @@
+const port = 8020;
+
+//Express
+const express = require("express");
+const app = express();
+app.set('view engine', 'ejs');
+
+app.get("/", (request, response) => {
+    response.render("index"); // views/index.ejs
+});
+
+app.get("/formulario", (request, response) => {
+    response.send(
+        `<div style="
+        position: absolute;
+        background-color: orangered;
+        border: solid 8px black;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 50%;
+        height: 90%;
+        text-align: center;
+        font-size: 200%;">
+    <p>Nome: ${request.query.nome}</p>
+    <p>Sobrenome: ${request.query.sobrenome}</p>
+</div>`
+    );
+});
+
+app.use((request, response, next) => {
+    response.status(404).send("<h1>Página não encontrada.</h1>");
+})
+
+app.listen(port, () => {
+    console.log(`Servidor funcionando na porta: ${port}`);
+});
